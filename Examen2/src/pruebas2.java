af75ef75bc55874d6f0b6b2c183dabdcde1ad496
Noelia <Noelia@Daniel>
2019-05-26 10:05:37 +0200

    en esta version muestra por la consola mi nombre: Daniel zumaquero

--- Examen2/src/pruebas2.java
+++ Examen2/src/pruebas2.java
@@ -3,7 +3,7 @@ public class pruebas2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hola mundo");
+        System.out.println("daniel zumaquero");
 	}
 
 }
